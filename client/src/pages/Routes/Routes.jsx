ae9866aabded1ae3bb1ad710e469d4f011ed48bd
Shad Azam <azamshad08@gmail.com>
2024-05-02 17:46:13 +0530

    update

--- client/src/pages/Routes/Routes.jsx
+++ client/src/pages/Routes/Routes.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import AfterLoginNavbar from '../AfterLoginNavBar/AfterLoginNavbar'
 import BNavBar from '../NavBar/BNavBar'
+import DashBoard from '../Dashboard/DashBoard';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import LandingPage from '../LandingPage/LandingPage';
 
 const Routes = () => {
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <>
+                <Route path='/' element={<LandingPage />} />
+                <Route path='/dashboard' element={<DashBoard />} />
+            </>
+        )
+    )
     return (
-        <div>
-            {/* <BNavBar /> */}
-            <AfterLoginNavbar />
-        </div>
+        <>
+            <RouterProvider router={router} />
+        </>
     )
 }
 
