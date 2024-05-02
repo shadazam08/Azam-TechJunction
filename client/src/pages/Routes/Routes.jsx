848e9fed38120fdb634fbe9ec48abfc7eba83e3e
Shad Azam <azamshad08@gmail.com>
2024-05-02 17:28:49 +0530

    update by shad azam

---
+++ client/src/pages/Routes/Routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AfterLoginNavbar from '../AfterLoginNavBar/AfterLoginNavbar'
+import BNavBar from '../NavBar/BNavBar'
+
+const Routes = () => {
+    return (
+        <div>
+            {/* <BNavBar /> */}
+            <AfterLoginNavbar />
+        </div>
+    )
+}
+
+export default Routes
